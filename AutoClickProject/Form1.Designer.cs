c2f285e415a3f3d821a74e3fa21bfa05e91ee003
SunDuckz <gui.bampi0@gmail.com>
2025-05-03 16:23:54 -0300

    organização e melhora nas operações matematicas

--- AutoClickProject/Form1.Designer.cs
+++ AutoClickProject/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             button1 = new Button();
             comboBox1 = new ComboBox();
+            trackBar1 = new TrackBar();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
             SuspendLayout();
             // 
             // button1
@@ -51,23 +55,61 @@
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(149, 23);
             comboBox1.TabIndex = 1;
-            comboBox1.SelectedItem = "Modo Preciso";
+            comboBox1.SelectedValue = "Modo Preciso";
+            // ;
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(172, 257);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(149, 45);
+            trackBar1.TabIndex = 2;
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 25;
+            trackBar1.Value = 0;
+            trackBar1.ValueChanged += label2toTrackbar;
+            // 
+            // label1
+            // 
+            label1.Font = new Font("Arial", 10F);
+            label1.Location = new Point(172, 219);
+            label1.Name = "label1";
+            label1.Size = new Size(88, 35);
+            label1.TabIndex = 3;
+            label1.Text = "Clicks Per Second";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.Location = new Point(266, 219);
+            label2.Name = "label2";
+            label2.Font = new Font("Arial", 15F,FontStyle.Bold);
+            label2.Size = new Size(35, 35);
+            label2.TabIndex = 4;
+            label2.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(537, 517);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(trackBar1);
             Controls.Add(comboBox1);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
         private ComboBox comboBox1;
+        private TrackBar trackBar1;
+        private Label label1;
+        private Label label2;
     }
 }
--- AutoClickProject/Form1.cs
+++ AutoClickProject/Form1.cs
@@ -23,9 +23,10 @@ namespace AutoClickProject
 
         private Random random = new(); // gera numeros aleatorios 
         private System.Windows.Forms.Timer timer = new(); // cria um temporizador para o clique automatico
-
+        private System.Windows.Forms.Timer timerSecundario = new(); // cria um temporizador para o clique automatico
         private int clickInterval; // intervalo de tempo entre os cliques em milissegundos
-
+        private List<int> clickIntervalList; // array para armazenar os intervalos de tempo entre os cliques
+        private int valor;
         public Form1()
         {
             InitializeComponent();
@@ -34,37 +35,56 @@ namespace AutoClickProject
 
 
 
-
         public async void startAutoClicker(object Sender, EventArgs e)
         {
+            int valorAntes = (trackBar1.Value - 1 <= 0) ? 1 : trackBar1.Value - 1 ;
+            int valorMeio = (trackBar1.Value <= 0) ? 1 : trackBar1.Value;
+            int valorDepois = (trackBar1.Value + 1 <= 0) ? 1 : trackBar1.Value + 1;
+
+            clickIntervalList = new List<int> {1000 / valorAntes, 1000 / valorMeio, 1000 / valorDepois };
+
+
             if (comboBox1.SelectedItem.ToString() == "Modo Preciso")
             {
-                // Modo preciso: clique a cada 100ms
-                clickInterval = 100;
+
+                clickInterval = clickIntervalList[1];
             }
             else if (comboBox1.SelectedItem.ToString() == "Modo Variado")
             {
-                // Modo variado: clique a cada 100-500ms
-                clickInterval = random.Next(100, 500);
+                timerSecundario.Start();
+                timerSecundario.Interval = 1000; 
+                timerSecundario.Tick += (s, args) =>
+                {
+                    // gera um numero aleatorio entre 0 e 2
+                    valor = random.Next(clickIntervalList.Count);
+                    // pega o intervalo de tempo aleatorio da lista
+                    clickInterval = clickIntervalList[valor];
+                };
             }
             // executa o clique
             timer.Start();
             timer.Interval = clickInterval; // define o intervalo de tempo entre os cliques
             timer.Tick += (s, args) =>
             {
+
                 Clicar(); // chama o método de clique
             };
 
-           
-               await Task.Delay(20000);
 
-               
-               timer.Stop(); // para o temporizador após 20 segundos
-               MessageBox.Show("O tempo de clique automático terminou.");
-              
-            }
-        
+            await Task.Delay(10000);
 
+
+            timer.Stop(); // para o temporizador após 20 segundos
+            timerSecundario.Stop();
+        }
+
+
+        public void label2toTrackbar(object sender,EventArgs e)
+        {
+            // converte o valor do trackbar para o label
+            label2.Text = trackBar1.Value.ToString();
+
+        }
 
         private void Clicar()
         {
